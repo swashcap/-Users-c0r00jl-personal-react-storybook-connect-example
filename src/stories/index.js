6090bd96190883c8d9e0875397723e154dbd8d88
Cory Reed <swashcap@gmail.com>
2017-12-20 15:01:46 -0800

    feat(pkg): wire PrimaryButton up to story

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Welcome } from '@storybook/react/demo';
+import PrimaryButton from '../components/PrimaryButton';
+import configureStore from '../store/configureStore';
+
+const store = configureStore();
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+storiesOf('Primary Button', module)
+  .addDecorator((render) => (
+    <Provider store={store}>
+      {render()}
+    </Provider>
+  ))
+  .add('default', () => (
+    <PrimaryButton
+      onPress={action('clicked')}
+    >
+      Primary Button
+    </PrimaryButton>
+  ))
+  .add('disabled', () => (
+    <PrimaryButton
+      disabled
+      onPress={action('clicked')}
+    >
+      Disabled
+    </PrimaryButton>
+  ));
